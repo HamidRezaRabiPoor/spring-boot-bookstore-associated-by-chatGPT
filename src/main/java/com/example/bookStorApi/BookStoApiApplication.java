298ddd54576c402415f347dda93d4d47458c8a92
HamidReza <rabipoorhamidreza@gmail.com>
2025-05-29 09:06:16 -0700

    my commit

---
+++ src/main/java/com/example/bookStorApi/BookStoApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookStorApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bookStorApi/BookStoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookStorApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
