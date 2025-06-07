25af8780a4d852c92787202287742b4b705f1e04
HamidReza <rabipoorhamidreza@gmail.com>
2025-06-07 08:36:36 -0700

    new commit

---
+++ src/main/java/com/example/bookStorApi/security/ConfigSecurityFilterChain.java
@@ -0,0 +1,39 @@
+package com.example.bookStorApi.security;
+
+import com.example.bookStorApi.repository.UserRepository;
+import com.example.bookStorApi.user.User;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class ConfigSecurityFilterChain {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(requests ->
+                requests.requestMatchers("/", "/all")
+                 .permitAll().anyRequest()
+                 .authenticated())
+                .formLogin(form ->
+                        form.loginPage("/login")
+                        .permitAll())
+                .logout(LogoutConfigurer::permitAll);
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepository){
+        return username -> {
+            User user = userRepository.findByUsername(username);
+            if(user != null) return  user;
+            throw new UsernameNotFoundException("no such user exists");
+        };
+    }
+}
--- src/main/java/com/example/bookStorApi/service/UserServiceRepositoryImpl.java
+++ src/main/java/com/example/bookStorApi/service/UserServiceRepositoryImpl.java
@@ -9,9 +9,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
-
+@Service
 public class UserServiceRepositoryImpl implements UserServiceRepository{
     private static final Logger LOGGER = LoggerFactory.getLogger(UserServiceRepositoryImpl.class);
 
